13f01435689fb5274ec7fa10df5d3460b4071dc4
Sai Sreya <144878524+soy0717@users.noreply.github.com>
2024-12-03 05:19:20 +0530

    day 2

--- day2/part1.py
+++ day2/part1.py
@@ -3,11 +3,14 @@ def safeOrUnsafe(file):
     safe = 0 
     for i in l:
         sub_list = [int(x) for x in i.split()]
-        distances = [abs(sub_list[x] - sub_list[x+1]) for x in range(len(sub_list)-1)]
+        # This check if the sequence is strictly increasing or decreasing
         if not (sub_list == sorted(sub_list) or sub_list == sorted(sub_list, reverse=True)):
             continue
+        # This check if two adjacent levels differ by at least 1 and at most 3
+        distances = [abs(sub_list[x] - sub_list[x+1]) for x in range(len(sub_list)-1)]
         if not all(x in [1,2,3] for x in distances):
             continue
+            
         safe += 1
     return safe
 
