bc1aec27f4a89ff4e6a8de41c69a6b70e1bdcebb
Sai Sreya <144878524+soy0717@users.noreply.github.com>
2024-12-03 05:25:52 +0530

    day2 and rename part2.py to day2-part2.py

--- day2/day2-part2.py
+++ day2/day2-part2.py
@@ -3,10 +3,13 @@ def safeOrUnsafe(file):
     safe = 0 
     for i in l:
         sub_list = [int(x) for x in i.split()]
+        # Function to check if a given sequence is safe
         def is_safe(seq):
             is_sorted = all(seq[x] < seq[x+1] for x in range(len(seq)-1)) or all(seq[x] > seq[x+1] for x in range(len(seq)-1))
             distances = all(y in [1,2,3] for y in [abs(seq[x] - seq[x+1]) for x in range(len(seq)-1)])
             return is_sorted and distances
+
+        # This checks is the sequence is safe with the new condition
         if is_safe(sub_list): 
             safe += 1
         else:
